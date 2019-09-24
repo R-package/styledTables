a189a2b5147406ce9bf54112ce6a05a29d5dd842
Adrian Maldet - B <maldet@xlwt0006.statistik.local>
2019-09-24 18:06:55 +0200

    Update logo

--- various/create_logo.R
+++ various/create_logo.R
@@ -1,16 +1,17 @@
-# create the poodle logo of the styledtable package
+# create the poodle logo of the styledTables package
 # install.packages("hexSticker")
 hexSticker::sticker(
-    "../man/figures/poodle.png",
-    package="styledtable", 
-    p_size=16,
-    p_y = 0.8,
-    s_x=1,
-    s_y=1.3,
-    h_fill = "#ff81ff",
-    p_color = "#fffafa",
-    h_color = "#fa61fa",
-    s_width=0.3,
-    s_height=0.3,
-    filename="../man/figures/logo.png"
-  )
+  "poodle.png",
+  package = c("styled   ", "    table"),
+  p_size = 22,
+  p_y = 0.7,
+  p_x = c(0.68, 1.26),
+  s_y = 1.3,
+  s_x = 1,
+  h_fill = "#ff81ff",
+  p_color = c("#e91e63", "#fffafa"),
+  h_color = "#fa61fa",
+  s_width = 0.4,
+  s_height = 0.4,
+  filename = "../man/figures/logo.png"
+)
